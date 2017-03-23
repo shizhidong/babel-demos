dffd6b385dd80a0cc98fe1dc8ccbc8dd6e74ec3f
unknown <you@example.com>
2017-03-23 15:11:12 +0800

    新的文件

---
+++ build/index.js
@@ -0,0 +1,22 @@
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define([], factory);
+  } else if (typeof exports !== "undefined") {
+    factory();
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory();
+    global.index = mod.exports;
+  }
+})(this, function () {
+  "use strict";
+
+  var sum = function sum(a, b) {
+    return a + b;
+  };
+  var x = sum(1, 2);
+  var v = sum(3, 4);
+  console.log(x);
+});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const sum = (a,b)=>a+b;
+let x = sum(1,2);
+let v = sum(3,4);
+console.log(x)
